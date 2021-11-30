0ff50850453255dd17bb95f82b5ffeff1ea25029
mehdisafari77 <sa.mehdisafari@gmail.com>
2021-11-29 20:25:15 -0800

    testing dplkoyment

---
+++ client/build/precache-manifest.982270fa492921c66143412451899814.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a8afa811acccbb24b61752ba1a0d86e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c6b954d3d7e99e4cbac",
+    "url": "/static/css/2.429e79c4.chunk.css"
+  },
+  {
+    "revision": "7c50c0ec5bcbd39e3bf0",
+    "url": "/static/css/main.a13d3d6a.chunk.css"
+  },
+  {
+    "revision": "2c6b954d3d7e99e4cbac",
+    "url": "/static/js/2.0d2cba81.chunk.js"
+  },
+  {
+    "revision": "4ebd2e1c8aec2794a625d08d8b03eea9",
+    "url": "/static/js/2.0d2cba81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c50c0ec5bcbd39e3bf0",
+    "url": "/static/js/main.a6eece7c.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
